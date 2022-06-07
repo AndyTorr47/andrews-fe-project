e2c824570d7d3ed4122e90e89ae61ac80994ac8d
Andrew Torr <andrewtorr1@outlook.com>
2022-06-07 16:49:17 +0100

    view a list of comments nearly working

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -17,19 +17,23 @@ function Articles() {
       <h1>Articles</h1>
       {articles?.articles?.map((article) => {
         return (
-          <li key={article.article_id}>
-            <h2>
-              <Link to={`articles/${article.article_id}`}>{article.title}</Link>
-            </h2>
-            <h3> Author: {article.author} </h3>
-            <h3>
-              Topic:
-              <Link to={`/topics/${article.topic}`}> {article.topic}</Link>{" "}
-            </h3>
-            <h3> Votes: {article.votes} </h3>
-            <h3> comments: {article.comment_count} </h3>
-            <p> created at: {article.created_at} </p>
-          </li>
+          <div className="card-box" key={article.article_id}>
+            <li className="card">
+              <h2>
+                <Link to={`articles/${article.article_id}`}>
+                  {article.title}
+                </Link>
+              </h2>
+              <h3> Author: {article.author} </h3>
+              <h3>
+                Topic:
+                <Link to={`/topics/${article.topic}`}>{article.topic}</Link>
+              </h3>
+              <h3> Votes: {article.votes} </h3>
+              <h3> comments: {article.comment_count} </h3>
+              <p> created at: {article.created_at} </p>
+            </li>
+          </div>
         );
       })}
     </section>
